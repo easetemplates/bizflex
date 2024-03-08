9c39dfe8c906b089cfa61855fd72af74c752e0bf
anitaparmar26 <easeanita@gmail.com>
2024-03-08 15:33:43 +0530

    uodated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,54 @@
-const path = require('path');
+import fs from 'fs';
+import { resolve } from 'path';
+
+// Function to dynamically generate entry points for a given directory
+function generateEntryPoints(dir) {
+	return fs
+		.readdirSync(dir)
+		.filter((file) => file.endsWith('.html'))
+		.reduce((acc, file) => {
+			const entryName = file.replace('.html', '');
+			acc[entryName] = resolve(__dirname, `${dir}/${file}`);
+			return acc;
+		}, {});
+}
+
+// Dynamically generate entry points for a single directory
+const allEntryPoints = generateEntryPoints('./src');
 
 export default {
-	base: './',
-	root: path.resolve(__dirname, 'src'),
-	build: {
-		outDir: '../dist',
-	},
+	root: './src',
 	server: {
-		port: 8080,
+		host: true,
+		port: 3000,
+		hot: true,
+		open: true,
+		force: true,
+		hmr: true,
 	},
+	plugins: [],
+	build: {
+		outDir: resolve(__dirname, 'dist'),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: allEntryPoints,
+			output: {
+				chunkFileNames: 'assets/js/theme.min.js',
+				entryFileNames: 'assets/js/theme.min.js',
+				assetFileNames: ({ name }) => {
+					if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+						return 'assets/images/[name][extname]';
+					}
+					if (/\.(woff|woff2|eot|ttf|otf)$/.test(name ?? '')) {
+						return 'assets/fonts/[name][extname]';
+					}
+					if (/\.css$/.test(name ?? '')) {
+						return 'assets/css/theme.min.css';
+					}
+					return 'assets/[name][extname]';
+				},
+			},
+		},
+	},
+	publicDir: '../public',
 };
